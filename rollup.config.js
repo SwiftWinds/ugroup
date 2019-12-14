f46d8bb3af0c83b91a65523c0bacf64e15af8269
Mateo Wang <SwiftWinds>
2019-12-13 16:25:24 -0800

    Add Navbar

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -48,6 +49,20 @@ export default {
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser(),
+
+    // to import scss files to .svelte files
+    postcss({
+      extract: true,
+      minimize: true,
+      use: [
+        ['sass', {
+          includePaths: [
+            './theme',
+            './node_modules'
+          ]
+        }]
+      ]
+    }),
   ],
   watch: {
     clearScreen: false,
---
+++ src/Navbar.svelte
@@ -0,0 +1,69 @@
+<script>
+  import "./Navbar.scss";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
+  import { Icon } from "@smui/common";
+
+  import {
+    mdiUndo,
+    mdiRedo,
+    mdiContentSave,
+    mdiShuffle,
+    mdiPower,
+    mdiCallSplit
+  } from "@mdi/js";
+</script>
+
+<TopAppBar variant="static" class="demo-top-app-bar mdc-elevation--z4">
+  <Row>
+    <Section align="start">
+      <Title href="/" class="mdc-theme--primary">μGroup</Title>
+    </Section>
+    <Section align="middle" toolbar>
+      <IconButton href="https://twitter.com/SciActive">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiUndo} />
+          </svg>
+        </Icon>
+      </IconButton>
+      <IconButton href="https://github.com/hperrin/svelte-material-ui">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiRedo} />
+          </svg>
+        </Icon>
+      </IconButton>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton href="https://twitter.com/SciActive">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiContentSave} />
+          </svg>
+        </Icon>
+      </IconButton>
+      <IconButton href="https://github.com/hperrin/svelte-material-ui">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiShuffle} />
+          </svg>
+        </Icon>
+      </IconButton>
+      <IconButton href="https://github.com/hperrin/svelte-material-ui">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiCallSplit} />
+          </svg>
+        </Icon>
+      </IconButton>
+      <IconButton href="https://github.com/hperrin/svelte-material-ui">
+        <Icon>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiPower} />
+          </svg>
+        </Icon>
+      </IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
